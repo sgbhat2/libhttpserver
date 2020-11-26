6c109e3f31c5a88fad52db57bf19c6f19878bc2d
Shilpasri G Bhat <sgbhat2@illinois.edu>
2020-11-26 00:13:39 -0600

    Support running multiple jobs

--- test/fuzz/basic_fuzzer.cc
+++ test/fuzz/basic_fuzzer.cc
@@ -28,21 +28,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
 
 #include <iostream>
 #include <sstream>
-#include <stdint.h>
-#include <stddef.h>
 #include <httpserver.hpp>
 
-#define	PORT	8080
+#define HOST_IP	"127.0.0.1"
+
+unsigned int get_port_no(void);
 
 using namespace httpserver;
-webserver ws = create_webserver(PORT).start_method(http::http_utils::INTERNAL_SELECT);
+unsigned int port;
+webserver ws = create_webserver(get_port_no());
 
 class fuzz_resource : public http_resource {
 public:
@@ -57,43 +60,63 @@ public:
 class args_resource: public http_resource {
 public:
 	const std::shared_ptr<http_response> render(const http_request& req) {
-		return std::shared_ptr<http_response>(new string_response("ARGS: " + req.get_arg("arg1") + "and" + req.get_arg("arg2")));
+		return std::shared_ptr<http_response>(new string_response("ARGS: " +
+					req.get_arg("arg1") + "and" + req.get_arg("arg2")));
 	}
 }agr;
 
-void error(const char *msg) {
+unsigned int get_port_no(void) {
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in address;
+	socklen_t len = sizeof(address);
+
+	memset(&address, 0 ,sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = inet_addr(HOST_IP);
+	address.sin_port = 0; //Use the next free port
+
+	bind(fd, (struct sockaddr*) &address, sizeof(address));
+	getsockname(fd, (struct sockaddr*) &address, &len);
+	port = ntohs(address.sin_port);
+
+	printf("Using port %d\n", port);
+	close(fd);
+	return port;
+}
+
+void quit(const char *msg) {
 	perror(msg);
-	exit(0);
+	exit(-1);
 }
 
 int connect_server(void) {
-	struct sockaddr_in serv_addr;
-	int sockfd, ret;
+	struct sockaddr_in address;
+	int sfd, ret;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		if (sockfd == -EINTR)
-			sockfd = socket(AF_INET, SOCK_STREAM, 0);
-		if (sockfd < 0)
-			error("ERROR opening socket");
+	sfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sfd < 0) {
+		if (sfd == -EINTR)
+			sfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sfd < 0)
+			quit("Failed to open socket");
 	}
 
-	memset(&serv_addr,0,sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
-	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	memset(&address,0,sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	address.sin_addr.s_addr = inet_addr(HOST_IP);
 
-	ret = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	ret = connect(sfd, (struct sockaddr *)&address, sizeof(address));
 	if (ret < 0) {
 		if (ret == -EINTR)
-			ret = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+			ret = connect(sfd, (struct sockaddr *)&address, sizeof(address));
 		if (ret < 0)
-			error("ERROR connecting");
+			quit("Failed to connect to server");
 	}
-	return sockfd;
+	return sfd;
 }
 
-void write_request(int sockfd, const uint8_t *data, size_t size) {
+void write_request(int sfd, const uint8_t *data, size_t size) {
 	std::string method = "PUT ";
 	std::string suffix = " HTTP/1.1\r\n\r\n";
 	std::string str(reinterpret_cast<const char *>(data), size);
@@ -104,24 +127,24 @@ void write_request(int sockfd, const uint8_t *data, size_t size) {
 	size = fstr.length();
 	msg = fstr.c_str();
 	do {
-		bytes = write(sockfd, msg + sent, size - sent);
-		if (bytes < 0)
-			error("ERROR writing message to socket");
+		bytes = write(sfd, msg + sent, size - sent);
+		if (bytes < 0 && bytes != -EINTR)
+			quit("Failed to write HTTP request");
 		if (bytes == 0)
 			break;
 		sent += bytes;
 	} while (sent < size);
 }
 
-void read_response(int sockfd) {
-	char response[512];
+void read_response(int sfd) {
+	char response[150];
 	int bytes;
 
-	bytes = read(sockfd,response , 200);
+	bytes = read(sfd, response , 150);
 	if (bytes < 0) {
 		// If interrupted try again
 		if (bytes == -EINTR)
-			bytes = read(sockfd,response , 200);
+			bytes = read(sfd, response , 150);
 	}
 #if PRINT_RESPONSE
 	printf("%s\n", response);
@@ -130,6 +153,7 @@ void read_response(int sockfd) {
 
 void cleanup(void)
 {
+	/* Stop the server */
 	ws.stop();
 }
 
@@ -137,36 +161,33 @@ extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv)
 {
 	ws.register_resource("{arg1|[A-Z]+}/{arg2|(.*)}", &agr);
 	ws.register_resource(R"(.*)", &hwr);
-	hwr.allow_all();
-	agr.allow_all();
+
 	/* Start the server */
 	ws.start(false);
+
 	atexit(cleanup);
+
 	return 0;
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-	int sockfd;
+	int sfd;
 
 	if (memchr(data, '\n', size))
 		return 0;
 
 	if (memchr(data, '\r', size))
 		return 0;
-	//if (size)
-		//ws.register_resource(reinterpret_cast<const char *>(data), &agr);
 
 	/* Client -> connect to server */
-	sockfd = connect_server();
+	sfd = connect_server();
 
-	/* HTTP request*/
-	write_request(sockfd, data, size);
-	read_response(sockfd);
+	/* HTTP request and response*/
+	write_request(sfd, data, size);
+	read_response(sfd);
 
 	/* Client -> close connection */
-	close(sockfd);
+	close(sfd);
 
-	/* Stop the server */
-	//ws.stop();
 	return 0;
 }
